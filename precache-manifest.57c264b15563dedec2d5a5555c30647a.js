13108b9c70f804631a181bc034c07beab6f9baf1
Luis Felipe Mejia Castrillon <luisfe_617@outlook.com>
2019-07-09 11:19:32 -0500

    Updates

---
+++ precache-manifest.57c264b15563dedec2d5a5555c30647a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b2e5b435006b3225af9302601a4cc3",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "3c969b95f026b011069e",
+    "url": "/connect-four/static/css/main.0ee0d366.chunk.css"
+  },
+  {
+    "revision": "1520e463f9eddcd51476",
+    "url": "/connect-four/static/js/2.9c56b0e7.chunk.js"
+  },
+  {
+    "revision": "3c969b95f026b011069e",
+    "url": "/connect-four/static/js/main.c9b252c3.chunk.js"
+  },
+  {
+    "revision": "5f78906a733342f2d383",
+    "url": "/connect-four/static/js/runtime~main.000e00dd.js"
+  },
+  {
+    "revision": "befdcbec56277690eba2c10327abda46",
+    "url": "/connect-four/static/media/logo.befdcbec.svg"
+  }
+]);
